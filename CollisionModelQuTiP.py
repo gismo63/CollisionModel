5e23f7af4961362fcb19f654f3b9b13bdec73a2c
gismo63 <oconnoreoin00@gmail.com>
2024-01-17 15:48:05 +0000

    Switched to Jupyter notebook and added full collision model

--- CollisionModelQuTiP.py
+++ CollisionModelQuTiP.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+"""Switched to Jupyter notebook"""
+
 def Hxyz(N, Jx, Jy, Jz, b, periodic=False):
     """Generates an XYZ spin chain Hamiltonian with N spins"""
     J_list = [Jx, Jy, Jz]
@@ -24,7 +26,7 @@ def Hxyz(N, Jx, Jy, Jz, b, periodic=False):
             Ham += J_list[j]*tensor(spin_list)
     return Ham
 
-def collision_model_dynamics(num_col, rho_S_0, rho_A, expec_list, H_S, H_int, t, gamma, dt):
+def collision_model_dynamics(num_col, rho_S_0, rho_A, expec_list_S, expec_list_SA, H_S, H_int, t, gamma, dt):
     """Simulates collision model dynamics and returns expectation values at each step"""
     rho_S = rho_S_0
     state = tensor(rho_S_0, rho_A)
@@ -35,9 +37,9 @@ def collision_model_dynamics(num_col, rho_S_0, rho_A, expec_list, H_S, H_int, t,
     times_free = np.linspace(dt, free_steps[0]*dt, int(free_steps[0]))
     times_col = np.linspace(dt, col_step*dt, col_step)
 
-    res_free = mesolve(H_S, rho_S, times_free, [], [])
-    res_col = mesolve(H_col, state, times_col, [], [])
-    return res_col
+    res_free = mesolve(H_S, rho_S, times_free, [], expec_list_S)
+    res_col = mesolve(H_col, state, times_col, [], expec_list_SA)
+    return res_free, res_col
 
     
 
@@ -60,14 +62,17 @@ psi_0 = tensor([rand_ket_haar(2) for _ in range(num_spins)])
 rho_S_0 = psi_0 * psi_0.dag()
 rho_A = basis(2,0) * basis(2,0).dag()
 
-expec_list = [tensor([sigmax() if k == i else qeye(2) for k in range(num_spins)]) 
+expec_list_S = [tensor([sigmax() if k == i else qeye(2) for k in range(num_spins)]) 
+              for i in range(num_spins)]
+expec_list_SA = [tensor([sigmax() if k == i else qeye(2) for k in range(num_spins )] + [qeye(2)]) 
               for i in range(num_spins)]
 
 H_S = -Hxyz(num_spins, J, J*gamma, J*Delta, bfield, periodic=True)
 H_SA = 0.5*(tensor(sigmax(), *[qeye(2) for _ in range(num_spins - 1)], sigmax()) 
           + tensor(sigmay(), *[qeye(2) for _ in range(num_spins - 1)], sigmay()))
 
-out = collision_model_dynamics(num_collision, rho_S_0, rho_A, expec_list, H_S, H_SA, collision_duration, gamma, timestep)
+out_free, out_col = collision_model_dynamics(num_collision, rho_S_0, rho_A, expec_list_S, expec_list_SA, 
+                               H_S, H_SA, collision_duration, gamma, timestep)
 
-print(out.states[-1])
+print(out_free.states[-1])
 #print(np.average(out), gamma/timestep)
